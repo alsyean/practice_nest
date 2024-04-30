eb45be2279e403baec2f0a547d3c69cda5cefbe2
alsyean <alsyean@gmail.com>
2024-04-30 21:13:30 +0900

    nest controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Redirect, Req } from "@nestjs/common";
+import { Request } from "express";
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  // @HttpCode(200) HttpCode 지정 가능
+  // @Redirect('https://nestjs.com', /**/301) // 리다이렉트 시킬 수도 있음
+  getHello(@Req() req: Request, @Body() Body, @Param() Param): string {
+    console.log(req);
     return this.appService.getHello();
   }
 }
